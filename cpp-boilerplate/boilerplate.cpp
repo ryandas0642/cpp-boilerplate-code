dc582ee34419ff2f966156061f74debfac5c8416
ryandas0642 <118935276+ryandas0642@users.noreply.github.com>
2023-04-03 21:45:30 -0500

    Add files via upload

---
+++ cpp-boilerplate/boilerplate.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define ull unsigned long long
+
+#define pb push_back
+#define del erase
+#define int_max INT_MAX
+#define int_min INT_MIN
+
+const long double pi = 3.141592653589793238;
+const long double posphi = 1.618033988749894848;
+const long double negphi = -0.61803398875;
+const long double e = 2.718281828459045235;
+
+// fib recurrence relation approximation: floor((phi^n / sqrt 5) + 0.5)
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t; cin >> t;
+    while(t--)
+    {
+        
+    }
+
+    exit(0);
+}
